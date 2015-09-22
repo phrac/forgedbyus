bc61e82767e85d863b4b80dffd726b5af6641058
Derek Musselmann <derek@disflux.com>
2015-09-21 19:32:32 -0500

    breakout the product modal

--- forgedbyus/views.py
+++ forgedbyus/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
+from products.models import Product
+
 def index(request):
     return render_to_response('index.html',
                               {
