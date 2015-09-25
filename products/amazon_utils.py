75c3dbabfe80cb58e3a3b50b37a60a64fc9ab384
Derek Musselmann <derek@disflux.com>
2015-09-25 15:33:14 -0500

    fetch and save amazon images

--- products/amazon_utils.py
+++ products/amazon_utils.py
@@ -1,6 +1,10 @@
 from amazon.api import AmazonAPI
 from django.conf import settings
-from products.models import Product
+import requests
+import tempfile
+
+from django.core import files
+from products.models import Product, ProductImage
 from affiliates.models import Affiliate
 
 import math
@@ -35,5 +39,39 @@ def get_asin(asin):
                           )
 
         product.save()
+        image = ProductImage(product_id=product.id)
+        lf, file_ext = fetch_image(az.large_image_url)
+        image_num = ProductImage.objects.filter(product_id=product.id).count() + 1
+        image.image.save("%s-%s.%s" % (product.product_id, image_num, file_ext), files.File(lf))
+        image.save()
+
 
     return product
+
+def fetch_image(url):
+    # Steam the image from the url
+    request = requests.get(url, stream=True)
+
+    # Get the filename from the url, used for saving later
+    file_name = url.split('/')[-1]
+    file_ext = file_name.split('.')[-1]
+
+    # Create a temporary file
+    lf = tempfile.NamedTemporaryFile()
+
+    # Read the streamed image in sections
+    for block in request.iter_content(1024 * 8):
+
+        # If no more file then stop
+        if not block:
+            break
+
+        # Write image block to temporary file
+        lf.write(block)
+    return lf, file_ext
+    # Create the model you want to save the image to
+    #image = Image()
+
+    # Save the temporary image to the model#
+    # This saves the model so be sure that is it valid
+    #image.image.save(file_name, files.File(lf))
