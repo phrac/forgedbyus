d826766c3579fd7e11640ad430a00f755bb62e5b
Derek Musselmann <derek@disflux.com>
2015-10-03 12:01:04 -0500

    remove white noise

--- forgedbyus/wsgi.py
+++ forgedbyus/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forgedbyus.settings")
 
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
