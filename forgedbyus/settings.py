4ea7ff6fc01295f33a9e8435986c4a0c71ece659
Derek Musselmann <derek@disflux.com>
2015-10-04 09:50:36 -0500

    damn STATIC_URL

--- forgedbyus/settings.py
+++ forgedbyus/settings.py
@@ -119,5 +119,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = os.environ.get('STATIC_URL')
+if STATIC_URL is None:
+    STATIC_URL = 'http://s3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME + '/'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
