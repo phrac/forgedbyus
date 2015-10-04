0bd34315409c630fbf452aeecaa2c90dcc1950d6
Derek Musselmann <derek@disflux.com>
2015-10-04 18:45:19 -0500

    Add privacy policy and disclaimer

--- forgedbyus/urls.py
+++ forgedbyus/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from products import urls as product_urls
 from products import views as product_views
@@ -9,6 +10,11 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^fbu/(?P<product_id>[A-Z0-9]{10})/(?P<slug>[-\w]+)/$', product_views.details, name='product'),
     url(r'^fbu/(?P<product_id>[A-Z0-9]{10})/$', product_views.details, name='product'),
+
+    url(r'^privacy/', TemplateView.as_view(template_name="privacy.html"), name='privacy'),
+    url(r'^disclaimer/', TemplateView.as_view(template_name="disclaimer.html"), name='disclaimer'),
+
+
     url(r'^search/', include('haystack.urls')),
     url(r'^products/', include(product_urls)),
     url(r'^admin/', include(admin.site.urls)),
