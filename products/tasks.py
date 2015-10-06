d19446721cfec49c6c96f3d10c8f3854cea0df2f
Derek Musselmann <derek@disflux.com>
2015-10-05 21:12:42 -0500

    Make sure there is actually objects to process

--- products/tasks.py
+++ products/tasks.py
@@ -15,16 +15,17 @@ def update_price():
     time_threshold = datetime.now() - timedelta(minutes=settings.UPDATE_PRICE_THRESHOLD)
     affiliate = Affiliate.objects.get(name='Amazon')
     products = Product.objects.filter(price_updated__lt=time_threshold, affiliate=affiliate)[:10]
-    asins = [p.asin for p in products]
-    asins_string = ','.join(asins)
+    if len(products) > 0:
+        asins = [p.asin for p in products]
+        asins_string = ','.join(asins)
 
-    amazon = AmazonAPI(settings.AWS_ACCESS_KEY_ID,
-                       settings.AWS_SECRET_ACCESS_KEY,
-                       settings.AWS_ASSOCIATE_TAG)
-    az = amazon.lookup(ItemId=asins_string)
-    for p in az:
-        if p.price_and_currency[0] is not None:
-            product = Product.objects.get(asin=p.asin)
-            product.current_price = int(math.ceil(p.price_and_currency[0]))
-            product.price_updated = timezone.now()
-            product.save()
+        amazon = AmazonAPI(settings.AWS_ACCESS_KEY_ID,
+                           settings.AWS_SECRET_ACCESS_KEY,
+                           settings.AWS_ASSOCIATE_TAG)
+        az = amazon.lookup(ItemId=asins_string)
+        for p in az:
+            if p.price_and_currency[0] is not None:
+                product = Product.objects.get(asin=p.asin)
+                product.current_price = int(math.ceil(p.price_and_currency[0]))
+                product.price_updated = timezone.now()
+                product.save()
