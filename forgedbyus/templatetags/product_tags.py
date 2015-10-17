6edf52181231001b9aaab1742c2d44caf8fcfca1
Derek Musselmann <derek@disflux.com>
2015-10-17 13:42:47 -0500

    Reorganize templates and add collection banners on all pages

--- forgedbyus/templatetags/product_tags.py
+++ forgedbyus/templatetags/product_tags.py
@@ -1,8 +1,13 @@
 from django import template
-from products.models import Product
+from products.models import Product, Collection
 
 register = template.Library()
 
 @register.simple_tag
 def product_count():
     return Product.objects.all().count()
+
+@register.inclusion_tag('collection_banners.html')
+def collection_banners():
+    collections = Collection.objects.filter(featured=True).order_by('-created')[:4]
+    return {'collections': collections}
