5f8043868caa174f034ec1a6b3b46a0eac5e4050
Derek Musselmann <derek@disflux.com>
2016-01-17 08:44:58 -0600

    remove debug; update copyright

--- forgedbyus/settings.py
+++ forgedbyus/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
-DEBUG = True
+#DEBUG = True
 
 # Application definition
 
