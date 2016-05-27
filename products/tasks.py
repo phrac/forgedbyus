41c9b50e98161fb0fd7812582019895c23be75d2
Derek Musselmann <derek@disflux.com>
2016-05-26 19:33:25 -0500

    add some debug info to product price task

--- products/tasks.py
+++ products/tasks.py
@@ -19,6 +19,7 @@ def update_price():
     time_threshold = timezone.now() - timedelta(minutes=settings.UPDATE_PRICE_THRESHOLD)
     affiliate = Affiliate.objects.get(name='Amazon')
     products = Product.objects.filter(price_updated__lt=time_threshold, affiliate=affiliate)[:10]
+    print "found %s products to update" % products.count()
     if len(products) > 0:
         asins = [p.asin for p in products]
         asins_string = ','.join(asins)
