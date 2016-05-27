4cce588b875c474f615078e56a791a7410475c86
Derek Musselmann <derek@disflux.com>
2016-05-27 07:41:23 -0500

    remove some debugging

--- products/tasks.py
+++ products/tasks.py
@@ -19,7 +19,6 @@ def update_price():
     time_threshold = timezone.now() - timedelta(minutes=settings.UPDATE_PRICE_THRESHOLD)
     affiliate = Affiliate.objects.get(name='Amazon')
     products = Product.objects.filter(price_updated__lt=time_threshold, affiliate=affiliate).order_by('price_updated')[:10]
-    print "found %s products to update" % products.count()
     if len(products) > 0:
         asins = [p.asin for p in products]
         asins_string = ','.join(asins)
@@ -35,9 +34,8 @@ def update_price():
             process_item(az)
 
 def process_item(item):
-    print "in process_item(item) subroutine"
     product = Product.objects.get(asin=item.asin)
-    
+
     if item.price_and_currency[0] is not None:
         old_price = product.current_price
         product.current_price = int(math.ceil(item.price_and_currency[0]))
